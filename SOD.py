159006088092a6494bf839839a83e98e59fb9d24
corentin.latimier17 <corentin.latimier17@gmail.com>
2024-10-25 16:34:34 -0400

    commit

--- SOD.py
+++ SOD.py
@@ -49,7 +49,7 @@ BC_LEFT = BoundaryCondition(type="O_order_extrapolation")
 BC_RIGHT = BoundaryCondition(type="O_oder_extrapolation")
 BCS = BoundaryConditions(BC_LEFT, BC_RIGHT)
 
-################################ Numerical Scheme #####################################
+################################ Numerical Schemes #####################################
 SCHEME_1 = MacCormack()
 SCHEME_2 = LaxWendroff()
 SCHEME_3 = BeamWarming(0.05, 2.5*0.05)
@@ -57,7 +57,7 @@ SCHEME_3 = BeamWarming(0.05, 2.5*0.05)
 ################################# Linear Solver #######################################
 LINEAR_SOLVER = DirectSolver()
 
-################################ Solver 1 (transient) ###################################
+################################ Solvers (transient) ###################################
 CFL = 0.5
 maxTime = 250
 files = ['output/SOD/rhoA.txt', 'output/SOD/u.txt', 'output/SOD/rhoEA.txt', 'output/SOD/pressure.txt', 'output/SOD/mach.txt']
